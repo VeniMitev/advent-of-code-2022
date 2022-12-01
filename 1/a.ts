430939e7a3a26735e7019e23d419e18c953e4f8e
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2022-12-01 01:16:30 -0800

    First day, first challenge completed

---
+++ 1/a.ts
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const data = fs.readFile('./1/input.txt', 'utf8', (err: any, data: any) => {
+    if (err) {
+        console.log(err)
+    } else {
+        const fileData = data.toString()
+        
+        let result = fileData.split('\r\n').join('/').split('/\n\s*\n/').join('||').split('//')
+
+        let calcArr: any = []
+
+        for (let i = 0; i < result.length; i++) {
+            const res = result[i].split('/')
+            const calc = res.reduce((acc: number, val: string) => {
+                const total = Number(val) + acc;
+                return total
+            }, 0)
+            calcArr.push(calc)
+        }
+
+        console.log(Math.max(...calcArr))
+    }
+})
